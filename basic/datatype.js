706db8f0ef4058d8ad01927a6fc13e0b2c01e23a
Hritikkumar2506 <144464803+Hritikkumar2506@users.noreply.github.com>
2024-04-14 08:20:21 +0000

    Datatype

---
+++ basic/datatype.js
@@ -0,0 +1,29 @@
+let score=true
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valuenumber=Number(score)
+console.log(typeof valuenumber);
+console.log(valuenumber);
+
+ // "33" => 33
+ // "33abc"=> NaN
+ // true => 1; false =>0
+
+ let loggedin="1"
+ let boolloggedin=Boolean(loggedin)
+ console.log(boolloggedin);
+
+ // 1 => true, 0 => false
+ // "" => false
+ // "hritik" => true
+
+
+ let string =33
+ let stringnum=String(string)
+ console.log(stringnum);
+console.log(typeof stringnum);
+
+// 33
+//  string

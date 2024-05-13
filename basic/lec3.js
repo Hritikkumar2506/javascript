a665bb91c6e6438e5cf9aae99130eeccd3147dfd
Hritikkumar2506 <144464803+Hritikkumar2506@users.noreply.github.com>
2024-05-13 14:12:18 +0000

    commit

--- basic/lec3.js
+++ basic/lec3.js
@@ -1,4 +1,27 @@
-let name="hritik"
-let age=18
-let isloggedin=true
-console.log(typeof "hritik")
+// let name="hritik"
+// let age=18
+// let isloggedin=true
+// console.log(typeof "hritik")
+// console.log("2" > 1);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null > 0);
+
+// Primitive datatypes 
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id=Symbol('123')
+const anoid=Symbol('123')
+console.log(id==anoid);
+
+const bignum=316165314613533534684n
+
+//Non Primitive
+// Array, Objects, Functions, 
+
+const hero=["god", "iron", "git"];
+
+const myFunction= function(){
+    console.log("hello world");
+}
+console.log(typeof hero);
